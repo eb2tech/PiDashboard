2e45f8489d82fd04cc2b0b4661b11bb7a3bd2efb
Edward Blakes <eblakes@eb2technologies.com>
2026-01-16 16:57:25 -0600

    Handle MQTT backlight/set commands and update discovery config

--- DashAgent/PiStateUpdater.cs
+++ DashAgent/PiStateUpdater.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Buffers;
+using System.Text.Json;
 using System.Text.Json.Nodes;
 using MQTTnet;
 
@@ -8,7 +9,7 @@ internal class PiStateUpdater(IConfiguration configuration, ILogger<PiStateUpdat
 {
     private PiController PiController => field ??= new PiController(logger);
     private PiModel PiModel => field ??= new PiModel();
-    
+
     private readonly string _mqttServer = configuration["Mqtt:Server"] ?? string.Empty;
     private readonly string _mqttUsername = configuration["Mqtt:Username"] ?? string.Empty;
     private readonly string _mqttPassword = configuration["Mqtt:Password"] ?? string.Empty;
@@ -21,14 +22,28 @@ internal class PiStateUpdater(IConfiguration configuration, ILogger<PiStateUpdat
                                                               .WithCredentials(_mqttUsername, _mqttPassword)
                                                               .Build();
 
+        mqttClient.ApplicationMessageReceivedAsync += (e) =>
+        {
+            var payload = System.Text.Encoding.UTF8.GetString(e.ApplicationMessage.Payload.ToArray());
+            var topic = e.ApplicationMessage.Topic;
+
+            return OnMqttMessage(mqttClient, payload, topic, stoppingToken);
+        };
+
         var result = await mqttClient.ConnectAsync(mqttClientOptions, stoppingToken);
         if (!mqttClient.IsConnected)
         {
-            logger.LogError("MQTT Client connected result: {ResultCode}, {Reason}", result.ResultCode, result.ReasonString);
+            logger.LogError("MQTT Client connected result: {ResultCode}, {Reason}", result.ResultCode,
+                            result.ReasonString);
         }
 
+        var topicPrefix = "raspi/" + PiController.DeviceId;
+        var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
+                                              .WithTopicFilter(topicPrefix + "/backlight/set").Build();
+        await mqttClient.SubscribeAsync(mqttSubscribeOptions, stoppingToken);
+
         await PublishDiscovery(mqttClient);
-        
+
         while (!stoppingToken.IsCancellationRequested)
         {
             Poll();
@@ -37,9 +52,46 @@ internal class PiStateUpdater(IConfiguration configuration, ILogger<PiStateUpdat
             await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
         }
 
-        var disconnectOptions = new MqttClientDisconnectOptionsBuilder().WithReason(MqttClientDisconnectOptionsReason.NormalDisconnection)
-                                                                        .Build();
+        var disconnectOptions = new MqttClientDisconnectOptionsBuilder()
+                                .WithReason(MqttClientDisconnectOptionsReason.NormalDisconnection)
+                                .Build();
         await mqttClient.DisconnectAsync(disconnectOptions, stoppingToken);
+    }
+
+    private Task OnMqttMessage(IMqttClient client, string payload, string topic, CancellationToken stoppingToken)
+    {
+        logger.LogInformation("Received Message: {Payload}", payload);
+        logger.LogInformation("  on topic: {Topic}", topic);
+
+        try
+        {
+            var json = JsonNode.Parse(payload);
+            if (json != null)
+            {
+                var state = json["state"]!.GetValue<string>();
+                switch (state)
+                {
+                    case "ON":
+                    {
+                        PiController.TurnOnDisplay();
+                        var brightness = json["brightness"]?.GetValue<int>() ?? PiController.GetMaxBrightness();
+                        PiController.SetBrightness(brightness);
+                        break;
+                    }
+                    case "OFF":
+                        PiController.TurnOffDisplay();
+                        break;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error processing backlight/set message");
+        }
+
+        _ = Task.Run(async () => await Publish(client), stoppingToken);
+
+        return Task.CompletedTask;
     }
 
     private async Task Publish(IMqttClient mqttClient)
@@ -47,43 +99,40 @@ internal class PiStateUpdater(IConfiguration configuration, ILogger<PiStateUpdat
         var piModel = PiModel;
 
         var backlightState = new JsonObject
-        {
-            ["state"] = piModel.IsOn ? "ON" : "OFF",
-            ["brightness"] = piModel.Brightness
-        };
+                             {
+                                 ["state"] = piModel.IsOn ? "ON" : "OFF",
+                                 ["brightness"] = piModel.Brightness
+                             };
 
         var topicPrefix = "raspi/" + PiController.DeviceId;
 
         var messages = new[]
-        {
-            new MqttApplicationMessageBuilder()
-                .WithTopic(topicPrefix+"/backlight/state")
-                .WithPayload(backlightState.ToJsonString())
-                .Build(),
-            new MqttApplicationMessageBuilder()
-                .WithTopic(topicPrefix+"/backlight/brightness/state")
-                .WithPayload(piModel.Brightness.ToString())
-                .Build(),
-            new MqttApplicationMessageBuilder()
-                .WithTopic(topicPrefix+"/cpu/usage")
-                .WithPayload(piModel.CpuUsage.ToString())
-                .Build(),
-            new MqttApplicationMessageBuilder()
-                .WithTopic(topicPrefix+"/cpu/temperature")
-                .WithPayload(piModel.CpuTemp.ToString())
-                .Build(),
-            new MqttApplicationMessageBuilder()
-                .WithTopic(topicPrefix+"/memory/usage")
-                .WithPayload(piModel.MemoryUsage.ToString())
-                .Build()
-        };
+                       {
+                           new MqttApplicationMessageBuilder()
+                               .WithTopic(topicPrefix + "/backlight/state")
+                               .WithPayload(backlightState.ToJsonString())
+                               .Build(),
+                           new MqttApplicationMessageBuilder()
+                               .WithTopic(topicPrefix + "/cpu/usage")
+                               .WithPayload(piModel.CpuUsage.ToString())
+                               .Build(),
+                           new MqttApplicationMessageBuilder()
+                               .WithTopic(topicPrefix + "/cpu/temperature")
+                               .WithPayload(piModel.CpuTemp.ToString())
+                               .Build(),
+                           new MqttApplicationMessageBuilder()
+                               .WithTopic(topicPrefix + "/memory/usage")
+                               .WithPayload(piModel.MemoryUsage.ToString())
+                               .Build()
+                       };
 
         foreach (var message in messages)
         {
             await mqttClient.PublishAsync(message);
         }
 
-        logger.LogDebug("Published state - Brightness: {Brightness}, IsOn: {IsOn}, CPU: {CpuUsage}%, Temp: {CpuTemp}°C, Memory: {MemoryUsage}%", 
+        logger.LogDebug(
+            "Published state - Brightness: {Brightness}, IsOn: {IsOn}, CPU: {CpuUsage}%, Temp: {CpuTemp}°C, Memory: {MemoryUsage}%",
             piModel.Brightness, piModel.IsOn, piModel.CpuUsage, piModel.CpuTemp, piModel.MemoryUsage);
     }
 
@@ -114,10 +163,11 @@ internal class PiStateUpdater(IConfiguration configuration, ILogger<PiStateUpdat
                                                                 ["unique_id"] = PiController.DeviceId + "_backlight_01",
                                                                 ["cmd_t"] = topicPrefix + "/backlight/set",
                                                                 ["stat_t"] = topicPrefix + "/backlight/state",
-                                                                ["bri_cmd_t"] = topicPrefix + "/backlight/brightness/set",
-                                                                ["bri_stat_t"] = topicPrefix + "/backlight/brightness/state",
+                                                                //["bri_cmd_t"] = topicPrefix + "/backlight/brightness/set",
+                                                                //["bri_stat_t"] = topicPrefix + "/backlight/brightness/state",
                                                                 ["schema"] = "json",
                                                                 ["brightness"] = true,
+                                                                ["brightness_scale"] = PiController.GetMaxBrightness(),
                                                                 ["name"] = "Backlight"
                                                             },
                                       ["cpu_usage"] = new JsonObject
@@ -156,18 +206,11 @@ internal class PiStateUpdater(IConfiguration configuration, ILogger<PiStateUpdat
                                          WriteIndented = false
                                      });
 
-        //var clearConfigMessage = new MqttApplicationMessageBuilder()
-        //                         .WithTopic("homeassistant/device/" + PiController.DeviceId + "/config")
-        //                         .WithPayload(string.Empty)
-        //                         .Build();
-
-        //await mqttClient.PublishAsync(clearConfigMessage);
-
         var message = new MqttApplicationMessageBuilder()
-            .WithTopic("homeassistant/device/" + PiController.DeviceId + "/config")
-            .WithPayload(json)
-            .WithRetainFlag()
-            .Build();
+                      .WithTopic("homeassistant/device/" + PiController.DeviceId + "/config")
+                      .WithPayload(json)
+                      .WithRetainFlag()
+                      .Build();
 
         await mqttClient.PublishAsync(message);
 
